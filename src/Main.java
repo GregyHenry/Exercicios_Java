46223450f3666eff88a7906af4a6e0715acc4ece
Greg <ryguimaraes@gmail.com>
2022-04-24 22:04:24 -0300

    Eclipse teste

--- src/Main.java
+++ src/Main.java
@@ -50,7 +50,7 @@ public class Main {
 		*/
 		
 		//Exercio emprestimo
-		double valor = 1400;
+		double valor = 1500;
 		int parcela = 5;
 		System.out.println("Exercicio Emprestimo");
 		Emprestimo.calcular(valor, Emprestimo.getDuasParcelas());
